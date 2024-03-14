c67cca596c56bb115f7edf620dff6a8f11a7c7b8
Farizd-Rusni <tujuhmocca@gmail.com>
2024-03-14 11:42:20 +0700

    4-MAR-24-FARIZD-RUSNI

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function Calculate() {
   var height = document.getElementById("h-input").value;
   var weight = document.getElementById("w-input").value;
 
-  var result = parseFloat(weight) / (parseFloat(height) / 100) ** 2;
+  var result = parseInt(weight) / (parseInt(height) / 100) ** 2;
 
   if (!isNaN(result)) {
     document.getElementById("bmi-output").innerHTML = result;
